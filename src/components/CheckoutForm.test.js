690ca38808d05e03d824ff860cba427d29d2b617
Daniel Brannon <daniel-brannon@hotmail.com>
2021-04-09 14:25:27 -0500

    done ez pz

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,33 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from "@testing-library/user-event";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm/>);
+    const header = screen.getByText(/checkout form/i)
+    expect(header).toBeInTheDocument();
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+    render(<CheckoutForm/>);
+    const firstNameInputField = screen.getByLabelText('First Name:');
+    userEvent.type(firstNameInputField, 'Daniel');
+    const lastNameInputField = screen.getByLabelText('Last Name:');
+    userEvent.type(lastNameInputField, 'Brannon');
+    const addressInputField = screen.getByLabelText('Address:');
+    userEvent.type(addressInputField, '123 Real Street');
+    const cityInputField = screen.getByLabelText('City:');
+    userEvent.type(cityInputField, 'RealCity');
+    const stateInputField = screen.getByLabelText('State:');
+    userEvent.type(stateInputField, 'RealState');
+    const zipInputField = screen.getByLabelText('Zip:');
+    userEvent.type(zipInputField, '12345');
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+
+    const successMessage = screen.getByText('You have ordered some plants! Woo-hoo!');
+    expect(successMessage).toBeInTheDocument();
+});
--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -9,10 +9,14 @@ export default class PlantList extends Component {
   //   - set the returned plants array to this.state.plants
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
+  state = {
+    plants: []
+  }
   componentDidMount() {
     axios.get('http://localhost:3333/plants')
       .then(res => {
         console.log(res);
+        this.setState({plants:res.data})
       });
   }
 
